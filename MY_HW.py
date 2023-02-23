e3501779aaeabe811d9813a69c8d9f1827695574
OleksandrSkr <skrigunaleksandr@gmail.com>
2023-02-23 17:19:13 +0200

    homework_3

--- MY_HW.py
+++ MY_HW.py
@@ -3,3 +3,8 @@ print(user_name.lower().capitalize())
 
 user_surname = input("Enter your surname : ")
 print(" ".join(user_surname).upper())
+
+user_age = input("How old are you ? ")
+import datetime
+current_yaer = datetime.datetime.now().year
+print("year of your birth", int(current_yaer) - int(user_age))
