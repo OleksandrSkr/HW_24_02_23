15942333e03a083777ff7fbc26fd7d47f7b8cdea
OleksandrSkr <skrigunaleksandr@gmail.com>
2023-02-23 17:56:30 +0200

    homework_4

--- MY_HW.py
+++ MY_HW.py
@@ -8,3 +8,11 @@ user_age = input("How old are you ? ")
 import datetime
 current_yaer = datetime.datetime.now().year
 print("year of your birth", int(current_yaer) - int(user_age))
+
+if int(user_age) < 13:
+    print("you are a child")
+if int(user_age) > 23:
+    print("you are an adult")
+else:
+    print("you are a teenager")
+
